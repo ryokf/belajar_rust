68b9a5e69d02b4452575bf98199119d30774bfd3
ryokf <ryokhrisnaf@gmail.com>
2025-03-04 05:59:06 +0700

    077-tipe data tuple

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,35 @@
 fn main() {
-    learn_06_data_type_char();
+    learn_07_data_type_tuple();
 }
 
-fn learn_06_data_type_char() {
-    // tipe data char
-    let c = 'z';
-    let z = 'ℤ';
-    let heart_eyed_cat = '😻';
+fn learn_07_data_type_tuple() {
+    // tipe data tuple
+    let data: (i32, f64, bool) = (500, 6.4, true);
+    println!("data: {:?}", data);
+
+    let a = data.0;
+    let b = data.1;
+    let c = data.2;
+    println!("a: {}", a);
+    println!("b: {}", b);
     println!("c: {}", c);
+
+    // destructuring
+    let (x, y, z) = data;
+    println!("\nhasil destructuring");
+    println!("x: {}", x);
+    println!("y: {}", y);
     println!("z: {}", z);
-    println!("heart_eyed_cat: {}", heart_eyed_cat);
+
+    // unit
+    // unit adalah tuple yang tidak memiliki elemen
+    // biasanya digunakan untuk mengembalikan nilai dari fungsi yang tidak mengembalikan nilai
+    let unit: () = ();
+    fn unit_func() -> () {
+        println!("unit function");
+    }
+    println!("\nnilai dari fungsi unit_func(): {:?}", unit_func());
+    println!("\nnilai dari variabel unit: {:?}", unit);
 }
 
 
