3fa49fa024951b4fab3df2d03e3dc6949279dbb8
ryokf <ryokhrisnaf@gmail.com>
2025-03-04 22:48:40 +0700

    11-tipe data string

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
 fn main() {
-    learn_10_memory_management();
+    learn_11_data_type_string();
 }
 
-fn learn_10_memory_management() {
-    // rust membagi memory menjadi 2 bagian, stack dan heap
-    // stack digunakan untuk menyimpan data yang ukurannya diketahui pada saat compile time
-    // heap digunakan untuk menyimpan data yang ukurannya tidak diketahui pada saat compile time
-    // stack lebih cepat dibandingkan heap karena stack menggunakan LIFO (Last In First Out) sedangkan heap menggunakan pointer
-    // stack memiliki ukuran yang terbatas, sedangkan heap tidak terbatas
-    
-    // rust langsung menghapus data yang disimpan di stack ketika data tersebut tidak digunakan lagi
-    // sedangkan data yang disimpan di heap tidak langsung dihapus ketika data tersebut tidak digunakan lagi
-    // data yang disimpan di heap akan dihapus ketika pointer yang menunjuk ke data tersebut dihapus
-    // pointer yang menunjuk ke data di heap disimpan di stack
+fn learn_11_data_type_string() {
+    // rust membagi string menjadi dua jenis, yaitu &str dan String
+    // &str adalah tipe data yang tidak bisa diubah, sedangkan String adalah tipe data yang bisa diubah
+    // &str disimpan di stack, sedangkan String disimpan di heap
+
+    let a = "ini adalah string menggunakan &str"; // &str
+    let b = String::from("ini adalah string menggunakan String"); // String
+
+    println!("{}", a);
+    println!("{}", b);
 }
 
 
