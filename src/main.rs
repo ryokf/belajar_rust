f105732c35a5e7d1393809847dc9e2f08f6b2b8a
ryokf <ryokhrisnaf@gmail.com>
2025-03-04 19:52:41 +0700

    09-constant

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,16 @@
 fn main() {
-    learn_08_data_type_array();
+    learn_09_constant();
 }
 
-fn learn_08_data_type_array() {
-    // tipe data array
-    // array adalah kumpulan data yang memiliki tipe data yang sama
-    let a = [1, 2, 3, 4, 5];
-    println!("a: {:?}", a);
-
-    // akses data array
-    let first = a[0];
-    let second = a[1];
-    println!("first: {}, second: {}", first, second);
-
-    // panjang array
-    // panjang array bertipe data usize karena maksimal panjang array sesuai dengan ukuran memori di sistem operasi
-    let len = a.len();
-    println!("len: {}", len);
-
-    // array 2 dimensi
-    let b: [[i32; 2]; 3] = [
-        [1, 2],
-        [3, 4],
-        [5, 6]
-    ];
-    println!("b: {:?}", b);
+fn learn_09_constant() {
+    // constant selalu immutable
+    // constant harus di-annotasi dengan tipe data
+    // penamaan constant menggunakan huruf kapital dan dipisa dengan underscore
+    const MAX_POINTS: i32 = 100000;
+    // const MIN_POINTS = 0; // error: cannot determine a type for this constant
+    println!("The value of MAX_POINTS is: {}", MAX_POINTS);
+    // MAX_POINTS = 100_001; // error: cannot assign to this expression
+    
 }
 
 
