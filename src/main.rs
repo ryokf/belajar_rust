42e222b59c2f4b8d7539fbe76efca1c56fb073f3
ryokf <ryokhrisnaf@gmail.com>
2025-03-06 05:47:57 +0700

    17-references and borrowing

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,43 @@
 fn main() {
-    learn_16_function_ownership();
+    learn_17_references_and_borrowing();
 }
 
-fn learn_16_function_ownership() {
-    fn test_func_data_stack(score: i32) {
-        println!("score di dalam function dari stack: {}", score);
+fn learn_17_references_and_borrowing() {
+    // dengan references kita bisa mengirimkan data ke function tanpa perlu mengirimkan data aslinya
+    // sehingga kita masih bisa menggunakan data aslinya
+    // proses ini disebut borrowing
+    // references menggunakan simbol &
+    fn full_name(first_name: &String, last_name: &String) -> String {
+        format!("{} {}", first_name, last_name)
+    }
+    let x = String::from("John");
+    let y = String::from("Doe");
+    let z = full_name(&x, &y);
+    println!("fisrt name: {}", x);
+    println!("last name: {}", y);
+    println!("full name: {}", z);
+
+    // secara default, references bersifat immutable
+    // sehingga kita tidak bisa mengubah data yang di reference
+    #[allow(unused)]
+    fn ubah_nama_error(nama: &String) {
+        // nama.push_str(" Doe"); // error
     }
 
-    fn test_func_data_heap(data: String) {
-        println!("data di dalam function dari heap: {}", data);
+    // jika kita ingin mengubah data yang di reference, kita bisa menggunakan mutable references
+    // mutable references menggunakan simbol &mut
+    fn ubah_nama(nama: &mut String) {
+        nama.push_str(" Doe");
     }
+    let mut nama = String::from("John"); // data asli yang direference harus mutable
+    ubah_nama(&mut nama);
+    println!("data yang diubah: {}", nama);
 
-    // data yang berada di stack ketika di passing ke function
-    // akan di copy ke dalam function tersebut
-    let angka = 10;
-    test_func_data_stack(angka);
-    println!("data angka masih bisa diakses : {}", angka);
-
-    // data yang berada di heap ketika di passing ke function
-    // akan di move ke dalam function tersebut
-    let data = String::from("Hello World");
-    test_func_data_heap(data);
-    // println!("data tidak bisa diakses : {}", data); // error
-
-    // ownership return value
-    fn test_func_return_value(value1: String, value2: String) -> String {
-        format!("{} {}", value1, value2)
-    }
-
-    let value1 = String::from("Hello");
-    let value2 = String::from("World");
-    let result = test_func_return_value(value1, value2); //ownership dari variable value1 dan value2 di move ke result
-    println!("hasil return function test_func_return_value: {}", result);
-    // println!("value1: {}", value1); // error
-    // println!("value2: {}", value2); // error
-
-    // mengembalikan ownership
-    fn test_func_return_ownership(x: String, y: String) -> (String, String, String) {
-        let result = format!("{} {}", x, y);
-        return (result, x, y);
-    }
-
-    let x = String::from("Hello");
-    let y = String::from("World");
-    let (result, x, y) = test_func_return_ownership(x, y);
-    println!("result: {}", result);
-    println!("x: {}", x);
-    println!("y: {}", y);
-
+    // mutable references hanya bisa ada satu pada satu waktu
+    // jika kita membuat dua mutable references, maka akan terjadi error
+    let mut nama = String::from("John");
+    #[allow(unused)]
+    let mut nama1 = &mut nama;
+    // let mut nama2 = &mut nama; // error: cannot borrow `nama` as mutable more than once at a time
+    println!("nama1: {}", nama1);
 }
